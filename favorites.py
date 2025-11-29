94c8413571e337bc4c36bcc5669a5f95ba963289
Shawn Evans <shawnevans328@gmail.com>
2025-11-28 19:15:45 -0500

    update

--- favorites.py
+++ favorites.py
@@ -1,10 +1,20 @@
 import csv
 
+from collections import Counter
+
+# Open CSV file
 with open("favorites.csv", "r") as file:
+
+    # Create DictReader
     reader = csv.DictReader(file)
 
-    next(reader) # skip first line of this file
+    # Counts
+    counts = Counter()
 
+    # iterate over CSV file, counting favorites
     for row in reader:
-        favorite = row[1]
-        print(favorite)
+        favorite = row["language"]
+        counts[favorite] += 1
+
+for favorite, count in counts.most_common():
+    print(f"{favorite}: {count}")
